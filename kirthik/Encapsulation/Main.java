e43c42ac9dd3b4aabb6d1c3f40f02bc233a363cb
kirthik023 <kirthikramsethu@gmail.com>
2024-10-21 16:48:16 +0530

    OOPS examples

---
+++ kirthik/Encapsulation/Main.java
@@ -0,0 +1,18 @@
+package Encapsulation;
+import java.util.*;
+class Main {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        person person=new person("kirthik",21);
+
+        System.out.println(person.getName());
+        System.out.println(person.getAge());
+        person.setName("Leo");
+        person.setAge(25);
+        System.out.println(person.getName());
+        System.out.println(person.getAge());
+
+
+    }
+    
+}
